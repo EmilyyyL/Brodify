6fc101a7f055858d7b3fe73418f8765160cab791
EmilyyyL <86397428+EmilyyyL@users.noreply.github.com>
2021-09-01 17:45:41 +1000

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,15 +1,32 @@
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    db.collection('guides').get().then(snapshot => {
+    db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
       setupUI(user);
-    });
+    }, err => console.log(err.message));
   } else {
     setupUI();
     setupGuides([]);
   }
-})
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
 
 // signup
 const signupForm = document.querySelector('#signup-form');
@@ -20,8 +37,15 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
+  // sign up the user & add firestore data
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      username: signupForm['signup-username'].value,
+      pronouns: signupForm['signup-pronouns'].value,
+      year: signupForm['signup-year'].value,
+      courses: signupForm['signup-courses'].value
+    });
+  }).then(() => {
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
