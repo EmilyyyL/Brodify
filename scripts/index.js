eabaa98d0fdac6c5a8bdafa42fbada1b1d8007a3
EmilyyyL <86397428+EmilyyyL@users.noreply.github.com>
2021-09-04 21:05:09 +1000

    Update and rename auth/index.js to scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
+const accountSettings = document.querySelector('.account-settings');
 
 const setupUI = (user) => {
   if (user) {
@@ -16,6 +17,45 @@ const setupUI = (user) => {
         <div>${doc.data().courses}</div>
       `;
       accountDetails.innerHTML = html;
+
+      const settingsHTML = `
+        <div> 
+          <p class="settings-headings">Username</p>
+          <p class="settings-txt"> 
+            ${doc.data().username} 
+            <a href="#" class="settings-btn modal-trigger" data-target="modal-change-username">Change username</a>
+          </p> 
+        </div>
+        <div> 
+          <p class="settings-headings">Email</p>
+          <p class="settings-txt">
+            ${user.email} 
+            <a href="#" class="settings-btn modal-trigger" data-target="modal-change-email">Change email</a>
+          </p> 
+        </div>
+        <div> 
+          <p class="settings-headings">Pronouns</p>
+          <p class="settings-txt">
+            ${doc.data().pronouns} 
+            <a href="#" class="settings-btn modal-trigger" data-target="modal-change-pronouns">Change pronouns</a>
+          </p> 
+        </div>
+        <div> 
+          <p class="settings-headings">Year</p>
+          <p class="settings-txt">
+            ${doc.data().year} 
+            <a href="#" class="settings-btn modal-trigger" data-target="modal-change-year">Change year</a>
+          </p> 
+        </div>
+        <div> 
+          <p class="settings-headings">Courses</p>
+          <p class="settings-txt"> 
+            ${doc.data().courses} 
+            <a href="#" class="settings-btn modal-trigger" data-target="change-courses">Edit courses</a>
+          </p> 
+        </div>
+      `;
+      accountSettings.innerHTML = settingsHTML;
     });
     // toggle user UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
@@ -23,6 +63,7 @@ const setupUI = (user) => {
   } else {
     // clear account info
     accountDetails.innerHTML = '';
+    accountSettings.innerHTML = '';
     // toggle user elements
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
@@ -31,7 +72,6 @@ const setupUI = (user) => {
 
 // setup guides
 const setupGuides = (data) => {
-
   if (data.length) {
     let html = '';
     data.forEach(doc => {
@@ -48,9 +88,15 @@ const setupGuides = (data) => {
   } else {
     guideList.innerHTML = '<p style="color:white">The homepage XD</p>';
   }
-  
-
 };
+
+// settings
+const closeSettings = document.querySelector('#close-settings');
+closeSettings.addEventListener('click', (e) => {
+  e.preventDefault();
+  const modal = document.querySelector('#modal-settings');
+  M.Modal.getInstance(modal).close();
+});
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
