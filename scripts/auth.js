ab2a1fec8611febabe78193a455b6e00cf3e4c88
EmilyyyL <86397428+EmilyyyL@users.noreply.github.com>
2021-09-07 17:05:32 +1000

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -36,42 +36,80 @@ auth.onAuthStateChanged(user => {
       });
     }
   } else {
+  // signup
+  const signupForm = document.querySelector('#signup-form');
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user & add firestore data
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      return db.collection('users').doc(cred.user.uid).set({
+        username: signupForm['signup-username'].value,
+        pronouns: signupForm['signup-pronouns'].value,
+        year: signupForm['signup-year'].value,
+        courses: signupForm['signup-courses'].value,
+        emailVerified: false
+      }, {merge:true});
+    }).then(() => {
+      // send email verification
+      firebase.auth().currentUser.sendEmailVerification().then(function() {
+        // Email sent.
+      }).catch(function(error) {
+        // An error happened.
+      });
+      // close the signup modal & reset form
+      const modal = document.querySelector('#modal-signup');
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+      window.location = 'homepage.html';
+    });
+  });
+  // login
+  const loginForm = document.querySelector('#login-form');
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+      // close the login modal & reset form
+      const modal = document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+      window.location = 'homepage.html';
+    });
+  });
+
+  // Forgot password
+  const forgotpasswordForm = document.querySelector('#forgot-password-form');
+  forgotpasswordForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = forgotpasswordForm['forgot-password-email'].value;
+
+    auth.sendPasswordResetEmail(email).then(function() {
+      // Email sent.
+      console.log('Email Sent');
+      // close the forgot password modal & reset form
+      const modal = document.querySelector('#modal-forgot-password');
+      M.Modal.getInstance(modal).close();
+      forgotpasswordForm.reset();
+    }).catch(function(error) {
+      // An error happened.
+    });
+  });
     setupUI();
     setupGuides([]);
     verifyEmailBanner.forEach(item => item.style.display = 'none');
   }
-});
-
-// signup
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
-
-  // sign up the user & add firestore data
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    return db.collection('users').doc(cred.user.uid).set({
-      username: signupForm['signup-username'].value,
-      pronouns: signupForm['signup-pronouns'].value,
-      year: signupForm['signup-year'].value,
-      courses: signupForm['signup-courses'].value,
-      emailVerified: false
-    }, {merge:true});
-  }).then(() => {
-    // send email verification
-    firebase.auth().currentUser.sendEmailVerification().then(function() {
-      // Email sent.
-    }).catch(function(error) {
-      // An error happened.
-    });
-    // close the signup modal & reset form
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
-  });
 });
 
 // logout
@@ -79,44 +117,5 @@ const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
   auth.signOut();
+  window.location = 'index.html';
 });
-
-// login
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
-
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    // close the login modal & reset form
-    const modal = document.querySelector('#modal-login');
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
-  });
-
-});
-
-// Forgot password
-const forgotpasswordForm = document.querySelector('#forgot-password-form');
-forgotpasswordForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = forgotpasswordForm['forgot-password-email'].value;
-
-  auth.sendPasswordResetEmail(email).then(function() {
-    // Email sent.
-    console.log('Email Sent');
-    // close the forgot password modal & reset form
-    const modal = document.querySelector('#modal-forgot-password');
-    M.Modal.getInstance(modal).close();
-    forgotpasswordForm.reset();
-  }).catch(function(error) {
-    // An error happened.
-  });
-});
-
